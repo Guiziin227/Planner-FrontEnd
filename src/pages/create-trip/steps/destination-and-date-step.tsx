1151d1c88037959a28ed76cbaa893f8d6e2fbe07
Guilherme Weber <gh63388@gmail.com>
2024-07-16 13:54:41 -0300

    Adicionando daypicker na home

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,5 +1,8 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from 'lucide-react'
+import { ArrowRight, Calendar, MapPin, Settings2, X } from 'lucide-react'
 import { Button } from '../../../components/button'
+import { useState } from 'react'
+import { DateRange, DayPicker } from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
 
 interface DestinationAndDateStepProps {
   isGuestInputOpen: boolean
@@ -12,6 +15,19 @@ export function DestinationAndDateStep({
   closeGuestInput,
   openGuestInput,
 }: DestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+  const [eventStartandEndDates, setEventStartandEndDates] = useState<
+    DateRange | undefined
+  >()
+
+  function openDatePicker() {
+    setIsDatePickerOpen(true)
+  }
+
+  function closeDatePicker() {
+    setIsDatePickerOpen(false)
+  }
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
       <div className="flex items-center gap-2 flex-1">
@@ -24,15 +40,40 @@ export function DestinationAndDateStep({
         />
       </div>
 
-      <div className="flex items-center gap-2">
+      <button
+        onClick={openDatePicker}
+        disabled={isGuestInputOpen}
+        className="flex items-center gap-2 text-left"
+      >
         <Calendar className="size-5 text-zinc-400" />
-        <input
-          disabled={isGuestInputOpen}
-          type="text"
-          placeholder="Quando?"
-          className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-        />
-      </div>
+        <span className="text-lg text-zinc-400 w-40">Quando?</span>
+      </button>
+
+      {isDatePickerOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold text-zinc-300">
+                  Selecione a data
+                </h2>
+                <button
+                  onClick={closeDatePicker}
+                  type="button"
+                  className="bg-transparent text-zinc-400 hover:bg-zinc-900"
+                >
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+            <DayPicker
+              mode="range"
+              selected={eventStartandEndDates}
+              onSelect={setEventStartandEndDates}
+            />
+          </div>
+        </div>
+      )}
 
       <div className="w-px h-6 bg-zinc-800" />
 
